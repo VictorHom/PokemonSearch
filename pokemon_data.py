03a95ef7890dddc3b906c020663836b2e5681beb
Victor Hom <vh62@cornell.edu>
2014-12-24 11:14:57 -0500

    Update pokemon_data.py

--- pokemon_data.py
+++ pokemon_data.py
@@ -1,18 +1,16 @@
 from sys import argv
 import pykemon
 import json
-#oython pokemon_data.py test.txt
+
 script = argv
 
 target = open("test.json", 'w')
 
 i = 1
 #make requests to the pokemon api to create a json object
-#that stores the pokemon, the abilities, types, and male-female ratio
 datas = []
 try:
 	while True:
-	# while True:
 		dataobj = {}
 		p = pykemon.get(pokemon_id= i)
 		dataobj["name"] = str(p.name)
